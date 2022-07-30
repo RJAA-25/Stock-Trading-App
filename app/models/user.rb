063a8253e12330530dcdd2dd12cf27a78fef9505
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-07-30 12:37:54 +0800

    Add initial User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :transactions
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :role, presence: true, inclusion: { in: ["trader", "admin"] }
+  validates :balance, presence: true, comparison: { greater_than_or_equal_to: 0 }
 end
