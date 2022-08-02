af11f8bb42396f45a66871f2e8c9fa14bf747dd4
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-08-02 22:59:40 +0800

    Generate user model with scoped controllers and views thru devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ApplicationRecord
-  has_many :transactions
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :role, presence: true, inclusion: { in: ["trader", "admin"] }
-  validates :balance, presence: true, comparison: { greater_than_or_equal_to: 0 }
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
 end
