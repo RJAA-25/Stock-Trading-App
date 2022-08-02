dc1a1007d51187bdbee63a93481689f6c99a9e06
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-08-03 02:32:28 +0800

    Customize user sessions and registrations controllers

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,16 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    redirect_to new_user_session_path, status: :see_other
+  end
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  
+  protected
+  def after_sign_in_path_for(resource)
+    trader_dashboard_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  devise_for :users, module: "users"
+  devise_for :users, module: "users", skip: [:sessions]
+  devise_scope :user do
+    get "login" => "users/sessions#new", as: :new_user_session
+    post "login" => "users/sessions#create", as: :user_session
+    delete "logout" => "users/sessions#destroy", as: :destroy_user_session
+    get "register" => "users/registrations#new", as: :register
+  end
 end
