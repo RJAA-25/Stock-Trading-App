066c197084ff345061409bc7fbfb81c125eada38
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-08-02 22:40:19 +0800

    Rollback migrations and start with empty schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_30_040241) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_130638) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "transactions", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "type"
-    t.string "status"
-    t.decimal "amount", precision: 10, scale: 2
-    t.decimal "price", precision: 10, scale: 2
-    t.decimal "total_cost", precision: 10, scale: 2
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "role"
-    t.decimal "balance", precision: 10, scale: 2, default: "0.0"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
