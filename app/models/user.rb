1f8b77140ab9d5225c3d60e32228e9016a5cab4d
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-08-03 01:28:53 +0800

    Add user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :role, presence: true, inclusion: { in: ["trader", "admin"] }
+  validates :status, presence: true, inclusion: { in: ["pending","approved"] }
+  validates :balance, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
