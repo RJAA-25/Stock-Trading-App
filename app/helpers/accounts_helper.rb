59f9e45ab60a4689fc02aff3946c65c44e6c48fc
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-08-07 03:22:13 +0800

    Implement conditional admin dashboard notice

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,9 +1,9 @@
 module AccountsHelper
-  def pending_trader_count(traders)
+  def pending_count(traders)
     traders.count { |trader| trader.status == "pending" }
   end
   
-  def approved_trader_count(traders)
+  def approved_count(traders)
     traders.count { |trader| trader.status == "approved" }
   end
 end
