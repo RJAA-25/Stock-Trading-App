de28d7ae37bdf01663fc7046917f964aa82e0427
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-08-07 22:59:17 +0800

    Remove precision and scale for decimal data types

--- db/migrate/20220802154210_add_attributes_to_users.rb
+++ db/migrate/20220802154210_add_attributes_to_users.rb
@@ -4,6 +4,6 @@ class AddAttributesToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :last_name, :string
     add_column :users, :role, :string, default: "trader"
     add_column :users, :status, :string, default: "pending"
-    add_column :users, :balance, :decimal, precision: 10, scale: 2, default: 0.00
+    add_column :users, :balance, :decimal, default: 0.0
   end
 end
