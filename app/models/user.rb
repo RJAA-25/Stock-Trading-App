cc4d9fd709e92ee12e4e4ee14ef42a95390abeac
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-08-09 18:28:58 +0800

    Generate Transaction model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   has_one :portfolio, dependent: :destroy
   has_many :properties, through: :portfolio
+  has_many :transactions, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
