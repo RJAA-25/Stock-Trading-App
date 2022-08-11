8f83efe8094863a5854fe19aff8c5d546da235b3
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-08-11 18:04:55 +0800

    Add default scope to Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,4 +5,6 @@ class Transaction < ApplicationRecord
   validates :quantity, presence: true 
   validates :price, presence: true 
   validates :total_amount, presence: true 
+
+  default_scope { order(created_at: :desc) }
 end
