ccfbd277bf78c12c6e42408e4bddd9d05442ec45
exceljake <aedceljake@gmail.com>
2022-08-12 19:21:29 +0800

    finish overall_worth of portfolio

--- app/controllers/traders_controller.rb
+++ app/controllers/traders_controller.rb
@@ -1,10 +1,23 @@
 class TradersController < ApplicationController
   before_action :authenticate_user!
   before_action :trader_access_restriction
+
+  # def overall_worth(id)
+  def portfolio_overall_worth
+        # @properties = Portfolio.find(id).properties 
+        overall_worth = 0 
+        # @properties.each do |prop| 
+        current_user.portfolio.properties.each do |prop|
+        price = Stock.find(prop.stock_id).latest_price 
+        overall_worth += prop.quantity * price 
+      end 
+    overall_worth
+  end 
   
   def dashboard
     @portfolio =  current_user.portfolio
     @transactions = current_user.transactions.first(3)
+    @portfolio.update(overall_worth: portfolio_overall_worth)
   end
 
   def portfolio
